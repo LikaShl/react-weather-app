8ddeec517d04535ab50861d8e354e5102fe5d92e
LikaShl <AnzhelikaShleiko@gmail.com>
2023-11-27 10:30:10 +0100

    add footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather app</div>;
+  return (
+    <div className="App">
+      <h1>Weather app</h1>
+      <footer>
+        This project was coded by Anzhelika Shleiko and is{""}
+        <a
+          href="https://weather-app-with-shecodes.netlify.app/"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
