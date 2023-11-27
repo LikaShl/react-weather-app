0d68e79b4eef5904ee20d7eb315ff5e5ddea9581
LikaShl <AnzhelikaShleiko@gmail.com>
2023-11-27 10:51:08 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import "./App.css";
 
 function App() {
@@ -5,12 +6,12 @@ function App() {
     <div className="App">
       <h1>Weather app</h1>
       <footer>
-        This project was coded by Anzhelika Shleiko and is{""}
+        This project was coded by Anzhelika Shleiko and is
         <a
           href="https://weather-app-with-shecodes.netlify.app/"
           target="_blank"
         >
-          open-sourced on GitHub
+          {""}open-sourced on GitHub
         </a>
       </footer>
     </div>
