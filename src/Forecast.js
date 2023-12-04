39b8a7ccb58d7e0b7a5a144bbe00e41ac82e8182
LikaShl <AnzhelikaShleiko@gmail.com>
2023-12-04 12:40:48 +0100

    add api for daily Forrecast

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,20 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import WeatherIcon from "./WeatherIcon";
 import "./Forecast.css";
+import axios from "axios";
 
-export default function Forecast() {
-  return (
-    <div className="Forecast">
-      <div className="row">
-        <div className="col-2 forecast-one-day">
-          <div forecast-day>Sat</div> <div forecast-date>2/12</div>
-          <WeatherIcon code={"01d"} />
-          <div className="forecast-tempreatures">
-            <span forecast-temperature-max>5</span>{" "}
-            <span forecast-temperature-min>-3</span>
+export default function Forecast(props) {
+  let [loaded, setLoaded] = useState(false);
+  let [forecastData, setForecastData] = useState(null);
+  function handleResponse(response) {
+    setForecastData(response.data.daily);
+    setLoaded(true);
+  }
+
+  if (loaded) {
+    console.log(forecastData);
+    return (
+      <div className="Forecast">
+        <div className="row">
+          <div className="col-2 forecast-one-day">
+            <div forecast-day>Sat</div> <div forecast-date>2/12</div>
+            <WeatherIcon code={"01d"} />
+            <div className="forecast-tempreatures">
+              <span forecast-temperature-max>{forecastData[0].temp.max}</span>{" "}
+              <span forecast-temperature-min>{forecastData[0].temp.min}</span>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "b40b135798f82a05aed08769f9275f50";
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/forecast/daily?lat=${latitude}&lon=${longitude}&cnt={6}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+    return null;
+  }
 }
--- src/Weather.js
+++ src/Weather.js
@@ -11,6 +11,7 @@ export default function Weather(props) {
   function handleResponse(response) {
     setWeatherData({
       ready: true,
+      coordinates: response.data.coord,
       temperature: response.data.main.temp,
       icon: response.data.weather[0].icon,
       humidity: response.data.main.humidity,
@@ -57,7 +58,7 @@ export default function Weather(props) {
           </div>
         </form>
         <CurrentWeather data={weatherData} />
-        <Forecast />
+        <Forecast coordinates={weatherData.coordinates} />
       </div>
     );
   } else {
